de15586e6b31d3b67e586ccd17e6e2caf059f7de
vagrant <vagrant@vagrant.vm>
2018-09-08 11:02:22 +0000

    add sh

---
+++ link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR=`dirname $0`
+cd $SCRIPT_DIR
+if [ "$(uname)" == "Darwin" ] ; then
+  ln -f ./karabiner/assets/complex_modifications/bundle_hyper_js_for_vim.json ~/.config/karabiner/assets/complex_modifications
+fi
